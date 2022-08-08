fc715ffab6009153d9cc6feab61f02f2d78ccb49
imsoosm <79003414+rithvikgrandhi@users.noreply.github.com>
2022-08-08 16:55:29 +0530

    Time: 5 ms (19.92%), Space: 5.8 MB (80.61%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -6,8 +6,13 @@ public:
          if (n==0) return 1;
         double t = myPow(x,n/2);
         if (n%2) {
-            return n<0 ? 1/x*t*t : x*t*t;
-        } else {
+            
+            if(n<0)
+                return 1/x*t*t;
+            else
+            return x*t*t;
+        } 
+        else {
             return t*t;
         }
         
