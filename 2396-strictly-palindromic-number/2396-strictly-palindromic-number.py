3fbdf2f59f646b5877b124985652570e3b831c5f
imsoosm <79003414+rithvikgrandhi@users.noreply.github.com>
2023-03-02 14:45:02 +0530

    Time: 30 ms (75.35%), Space: 13.8 MB (92.85%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+
+
+
+        for i in range(2,n-1):
+
+
+            num1=0
+
+            while n:
+
+                num1=num1*10+n%i
+                n=n//2
+            if str(num1)!=reversed(str(num1)):
+                return False
+            continue
+        return True
